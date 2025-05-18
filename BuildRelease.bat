06039e7aac8dd898f5700fe7a5a30af27363e95e
Alan Tse <alandtse@users.noreply.github.com>
2025-05-18 10:41:44 -0700

    ci: add compilation pr test and artifact posting (#1078)
    * ci: use lukka/run-cmake
    
    * ci: use build preset
    
    * ci: add pr building
    
    * ci: add pull request event
    
    * cI: refactor build.yaml
    
    * ci: use bash for shell
    
    * ci: remove pr-number
    
    * ci: save artifacts between jobs
    
    * ci: allow updates to existing tags
    
    * ci: remove unneeded GITHUB_TOKEN
    
    * ci: fix comment permissions
    
    * ci: fix pull_request_target event
    
    * ci: upsert the comment
    
    * ci: fix syntax error
    
    * ci: use build cache
    
    * ci: invalidate cache and replace artifacts
    
    * ci: remove old artifacts
    
    * build: use cmake build presets

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -9,7 +9,7 @@ echo Running preset %preset%
 
 cmake -S . --preset=%preset% --check-stamp-file "build\%preset%\CMakeFiles\generate.stamp"
 if %ERRORLEVEL% NEQ 0 exit 1
-cmake --build build/%preset% --config Release
+cmake --build --preset=%preset%
 if %ERRORLEVEL% NEQ 0 exit 1
 
 pause
